5f91057c9e6df8eb73468ed4aa447866fe25da5a
Patrick N <patricknnoonan@gmail.com>
2019-04-26 15:11:31 -0500

    Finished character validation function, utilized array.map function

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ Build all of your functions for displaying and gathering information below (GUI)
 */
 // app is the function called to start the entire application
 function app(people) {
+  let displayPeopleOption = promptFor("Would you like to see a list of available names? Enter 'yes' or 'no'", yesNo).toLowerCase();
+  if (displayPeopleOption == "yes"){
+  displayPeople(people);
+  }
   var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
   switch (searchType) {
     case 'yes':
@@ -13,7 +17,7 @@ function app(people) {
     case 'no':
       foundPerson = searchByTrait(people);
       mainMenu(foundPerson, people)
-      break;
+      break;   
     default:
       app(people); // restart app
       break;
@@ -25,18 +29,18 @@ function mainMenu(person, people) {
 
   /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
 
-    if (!person[0]) {
+  if (!person[0]) {
     alert("Could not find that individual.");
     return app(people); // restart
   }
-    if (person.length > 1) {
-        alert("More than one person was found with these traits. The list of people that match these traits are ")
-        for (let i = 0; i < person.length; i++) {
-            alert(person[i].firstName + ' '+ person[i].lastName);
-        };
-            return app(people);
-        
-    } 
+  if (person.length > 1) {
+    alert("More than one person was found with these traits. The list of people that match these traits are ")
+    for (let i = 0; i < person.length; i++) {
+      alert(person[i].firstName + ' ' + person[i].lastName);
+    };
+    return app(people);
+
+  }
   var displayOption = prompt("Found " + person[0].firstName + " " + person[0].lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
   let currentYear = new Date().getFullYear();
 
@@ -46,64 +50,65 @@ function mainMenu(person, people) {
 
 
 
-    switch (displayOption) {
-        case "immediate family":
+  switch (displayOption) {
+    case "immediate family":
 
-            let immedFamily = people.filter(function (person) {
-                if (person[0].id === firstName && person.lastName === lastName) {
-                    return true;
+      let immedFamily = people.filter(function (person) {
+        if (person[0].id === firstName && person.lastName === lastName) {
+          return true;
 
-
-            break;
+        }
+      });
+      break;
     case "info":
-          alert(person[0].firstName + person[0].lastName + " is a " + person[0].gender + ", who is " + age + " years old , and is " + person[0].height + " inches tall, aswell as an eye color of " + person[0].eyeColor + " and a weight of " + person[0].weight);
+      alert(person[0].firstName + person[0].lastName + " is a " + person[0].gender + ", who is " + age + " years old , and is " + person[0].height + " inches tall, aswell as an eye color of " + person[0].eyeColor + " and a weight of " + person[0].weight);
       break;
     case "family":
 
-          if (person[0].currentSpouse != null && person[0].parents.length == 2) {
-              alert(person[0].firstName + " " + person[0].lastName + " has a spouse with the id of " + person[0].currentSpouse + ", and parents with the id's of " + person[0].parents[0] + " and " + person[0].parents[1]);
-          } else if (person[0].currentSpouse != null && person[0].parents.length == 1) {
-              alert(person[0].firstName + " " + person[0].lastName + " has a spouse with the id of " + person[0].currentSpouse + ", and a parent with the id of " + person[0].parents[0]);
-          } else if (person[0].currentSpouse != null && person[0].parents.length == 0) {
-              alert(person[0].firstName + " " + person[0].lastName + " has a spouse with the id of " + person[0].currentSpouse + ", and the parents are unknown");
-          } else if (person[0].currentSpouse == null && person[0].parents.length == 2) {
-              alert(person[0].firstName + " " + person[0].lastName + " has parents with the id's of " + person[0].parents[0] + " and " + person[0].parents[1] + ", but has no spouse :( poor " + person[0].firstName + ".");
-          } else if (person[0].currentSpouse == null && person[0].parents.length == 1) {
-              alert(person[0].firstName + " " + person[0].lastName + " has parents with the id's of " + person[0].parents[0] + ", but has no spouse :( poor " + person[0].firstName + ".");
-          } else if (person[0].currentSpouse == null && person[0].parents.length == 0) {
-              alert(person[0].firstName + " " + person[0].lastName + " has unknown parents, and has no spouse. :( Poor " + person[0].firstName + ".");
-          }
+      if (person[0].currentSpouse != null && person[0].parents.length == 2) {
+        alert(person[0].firstName + " " + person[0].lastName + " has a spouse with the id of " + person[0].currentSpouse + ", and parents with the id's of " + person[0].parents[0] + " and " + person[0].parents[1]);
+      } else if (person[0].currentSpouse != null && person[0].parents.length == 1) {
+        alert(person[0].firstName + " " + person[0].lastName + " has a spouse with the id of " + person[0].currentSpouse + ", and a parent with the id of " + person[0].parents[0]);
+      } else if (person[0].currentSpouse != null && person[0].parents.length == 0) {
+        alert(person[0].firstName + " " + person[0].lastName + " has a spouse with the id of " + person[0].currentSpouse + ", and the parents are unknown");
+      } else if (person[0].currentSpouse == null && person[0].parents.length == 2) {
+        alert(person[0].firstName + " " + person[0].lastName + " has parents with the id's of " + person[0].parents[0] + " and " + person[0].parents[1] + ", but has no spouse :( poor " + person[0].firstName + ".");
+      } else if (person[0].currentSpouse == null && person[0].parents.length == 1) {
+        alert(person[0].firstName + " " + person[0].lastName + " has parents with the id's of " + person[0].parents[0] + ", but has no spouse :( poor " + person[0].firstName + ".");
+      } else if (person[0].currentSpouse == null && person[0].parents.length == 0) {
+        alert(person[0].firstName + " " + person[0].lastName + " has unknown parents, and has no spouse. :( Poor " + person[0].firstName + ".");
+      }
       break;
     case "descendants":
       let peopleToCheck = [person[0]];
       let allDescendants = [];
-    
+
       findChildren(peopleToCheck[0], peopleToCheck.length);
 
-      function findChildren(personChecking, count) {  
+      function findChildren(personChecking, count) {
 
-          if (count > 0){
+        if (count > 0) {
 
-            let childrenArray = people.filter(function(el){
-              if (el.parents[0] == personChecking.id || el.parents[1] == personChecking.id){
-                return true;
-              } else {
-                return false;
-              }
-            });
+          let childrenArray = people.filter(function (el) {
+            if (el.parents[0] == personChecking.id || el.parents[1] == personChecking.id) {
+              return true;
+            } else {
+              return false;
+            }
+          });
 
-            for (let i = 0; i < childrenArray.length; i++){
-              peopleToCheck.push(childrenArray[i])
-            } 
-            
-            allDescendants.push(personChecking.firstName + " " + personChecking.lastName);
-            count= peopleToCheck.length;
-            peopleToCheck.shift();
-
-            return findChildren(peopleToCheck[0], count-1);
-          } else {
-            alert(allDescendants[0] + "'s family tree is, " + allDescendants.join(", ") + ".");
+          for (let i = 0; i < childrenArray.length; i++) {
+            peopleToCheck.push(childrenArray[i])
           }
+
+          allDescendants.push(personChecking.firstName + " " + personChecking.lastName);
+          count = peopleToCheck.length;
+          peopleToCheck.shift();
+
+          return findChildren(peopleToCheck[0], count - 1);
+        } else {
+          alert(allDescendants[0] + "'s family tree is: " + allDescendants.join(", ") + ".");
+        }
       }
       break;
     case "restart":
@@ -191,7 +196,7 @@ function searchByTrait(people) {
   }
 
   var foundTrait = people.filter(function (person) {
-      if (person[chooseTraitOne] === firstTrait && person[chooseTraitTwo] === secondTrait) {
+    if (person[chooseTraitOne] === firstTrait && person[chooseTraitTwo] === secondTrait) {
       return true;
     }
     else {
@@ -210,11 +215,17 @@ function displayPeople(people) {
 }
 
 function displayPerson(person) {
+  //seems unnecessary?
   // print all of the information about a person:
   // height, weight, age, name, occupation, eye color.
   var personInfo = "First Name: " + person.firstName + "\n";
   personInfo += "Last Name: " + person.lastName + "\n";
-  // TODO: finish getting the rest of the information to display
+  personInfo += "Height: " + person.height + "\n";
+  personInfo += "Weight: " + person.weight + "\n";
+  //personInfo += "Age: " + person.age + "\n";
+  personInfo += "Occupation: " + person.occupation + "\n";
+  personInfo += "Eye color: " + person.eyeColor + "\n";
+
   alert(personInfo);
 }
 
@@ -233,7 +244,21 @@ function yesNo(input) {
 
 // helper function to pass in as default promptFor validation
 function chars(input) {
-  if ( input )
-  return true; // default validation only
+  if (input.match(/^[a-z0-9\/]+$/i)) {
+    return true; // default validation only
+  } else {
+    let invalidCharSwitch = prompt("You have entered invalid characters, would you like to try again? (yes = try again, no = restart program)")
+    switch (invalidCharSwitch) {
+      case 'no':
+        app(people); // restart app
+        break;
+      case 'yes':
+        return;
+      default:
+        app(people); // restart app
+        break;
+    }
+  }
 }
+
 
