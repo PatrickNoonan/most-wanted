33ca933b959a626b7162409fb5b828017da46cba
Patrick N <patricknnoonan@gmail.com>
2019-05-08 10:20:19 -0500

    Fixed assignment based on feedback

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ Build all of your functions for displaying and gathering information below (GUI)
 // app is the function called to start the entire application
 function app(people) {
   let displayPeopleOption = promptFor("Would you like to see a list of available names? Enter 'yes' or 'no'", yesNo).toLowerCase();
-  if (displayPeopleOption == "yes"){
-  displayPeople(people);
+  if (displayPeopleOption == "yes") {
+    displayPeople(people);
   }
   var searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
   switch (searchType) {
@@ -15,114 +15,128 @@ function app(people) {
       mainMenu(foundPerson, people);
       break;
     case 'no':
+      let traitNumToSearch = promptFor("Would you like to search by 1 trait or more?", chars);
+      if (traitNumToSearch == "1"){
       foundPerson = searchByTrait(people);
+      } else {
+        foundPerson = searchByTraits(people);
+      }
+      // pickATraitAndSearch(people);
       mainMenu(foundPerson, people)
-      break;   
+      break;
     default:
       app(people); // restart app
       break;
   }
 }
+/*---------------------------
+function pickATraitAndSearch(people)
+{
+  let displayTraitOption = promptFor("Would you like to see a list of available traits? Enter 'yes' or 'no'", yesNo).toLowerCase();
+  if (displayTraitOption == "yes"){
+    displayTraits(people);//display traits will go through all of people and create an array of unique traits it comes across
+    }
+    var searchType = promptFor("Do you know the trait of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
+    switch (searchType) {
+      case 'yes':
+        var foundPerson = searchByName(people);
+        mainMenu(foundPerson, people);
+        break;
+      case 'no':
+        foundPerson = searchByTrait(people);
+        mainMenu(foundPerson, people)
+        break;   
+      default:
+        app(people); // restart app
+        break;
+    }
+}
+
+function displayTraits(people)
+{
+  alert(people.filter(function (person) {
+    return person.gender + " " + person.height + " " + person.weight + " " + person.eyeColor + " " + person.occupation;
+  }).join("\n"));
+}
+/*
+array.map(item => item.age)
+  .filter((value, index, self) => self.indexOf(value) === index)
+
+  ------------------------------------------------------------*/
 
 // Menu function to call once you find who you are looking for
 function mainMenu(person, people) {
 
-    /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
+  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
 
 
-    if (!person[0]) {
-        alert("Could not find that individual.");
-        return app(people); // restart
-    }
-    if (person.length > 1) {
-        alert("More than one person was found with these traits. The list of people that match these traits are ")
-        for (let i = 0; i < person.length; i++) {
-            alert(person[i].firstName + ' ' + person[i].lastName);
-        };
-        return app(people);
+  if (!person[0]) {
+    alert("Could not find that individual.");
+    return app(people); // restart
+  }
+  if (person.length > 1) {
+    alert("More than one person was found with these traits. The list of people that match these traits are ")
+    for (let i = 0; i < person.length; i++) {
+      alert(person[i].firstName + ' ' + person[i].lastName);
+    };
+    return app(people);
 
-    }
-    var displayOption = promptFor("Found " + person[0].firstName + " " + person[0].lastName + " . Do you want to know their 'info', 'family', 'immediate family', or 'descendants'? Type the option you want or 'restart' or 'quit'",chars);
-    let currentYear = new Date().getFullYear();
+  }
+  var displayOption = promptFor("Found " + person[0].firstName + " " + person[0].lastName + " . Do you want to know their 'info', 'immediate family', or 'descendants'? Type the option you want or 'restart' or 'quit'", chars);
+  let currentYear = new Date().getFullYear();
 
-    let justYear = person[0].dob.split('/');
-    let age = currentYear - justYear[2];
-
-
-
+  let justYear = person[0].dob.split('/');
+  let age = currentYear - justYear[2];
 
 
 
-    switch (displayOption) {
-        case "immediate family":
-            //checks for parents of person and gives name
-            let immedParentsArray = people.filter(function (el) {
-                if (el.id == person[0].parents[0] || el.id == person[0].parents[1]) {
-                    return true;
-                } else {
-                    return false
-                }
-            });
-                    if (immedParentsArray.length > 0){
-                        for (let i = 0; i < immedParentsArray.length; i++) {
-                            alert(immedParentsArray[i].firstName + " " + immedParentsArray[i].lastName + " is the parent of " + person[0].firstName + " " + person[0].lastName + ".")
-                            }
-                        } else { alert(person[0] + " has no surviving parents.")};
-                
-            
 
 
-            //checks for children of person and gives name
-            let immedChildrenArray = people.filter(function (el) {
-                if (el.parents[0] == person[0].id || el.parents[1] == person[0].id) {
-                    return true;
-                } else {
-                    return false
-                }
-            });
-                if (immedChildrenArray.length > 0) {
-                    for (let i = 0; i < immedChildrenArray.length; i++) {
-                        alert(immedChildrenArray[i].firstName + " " + immedChildrenArray[i].lastName + " is the child of " + person[0].firstName + " " + person[0].lastName + ".")
-                    }
-                    } else { alert(person[0] + " has no surviving children." )}
-            
 
-            //checks for spouse of person and gives name
-            let immedSpouseArray = people.filter(function (el) {
-                if (el.spouse == person[0].id) {
-                    return true;
-                } else {
-                    return false
-                }
-            }); if (immedSpouseArray.length > 0) {
-                    for (let i = 0; i < immedChildrenArray.length; i++) {
-                        alert(immedSpouseArray[i].firstName + " " + immedSpouseArray[i].lastName + " is the spouse of " + person[0].firstName + " " + person[0].lastName + ".")
-                    }
-                } else { alert(person[0] + " has no surviving spouses.") }
-            
-            
-            
-         break;
+  switch (displayOption) {
+    case "immediate family":
+    let immedParentsArray = people.filter(function (el) {
+      if (el.id == person[0].parents[0] || el.id == person[0].parents[1]) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+    if (immedParentsArray.length > 0) {
+      for (let i = 0; i < immedParentsArray.length; i++) {
+        alert(immedParentsArray[i].firstName + " " + immedParentsArray[i].lastName + " is the parent of " + person[0].firstName + " " + person[0].lastName + ".")
+      }
+    } else { alert(person[0].firstName + " " + person[0].lastName + " has no surviving parents.") };
 
+    let immedChildrenArray = people.filter(function (el) {
+      if (el.parents[0] == person[0].id || el.parents[1] == person[0].id) {
+        return true;
+      } else {
+        return false
+      }
+    });
+    if (immedChildrenArray.length > 0) {
+      for (let i = 0; i < immedChildrenArray.length; i++) {
+        alert(immedChildrenArray[i].firstName + " " + immedChildrenArray[i].lastName + " is the child of " + person[0].firstName + " " + person[0].lastName + ".")
+      }
+    } else { alert(person[0].firstName + " " + person[0].lastName + " has no surviving children.") }
+
+    let immedSpouseArray = people.filter(function (el) {
+      if (el.currentSpouse == person[0].id) {
+        return true;
+      } else {
+        return false
+      }
+    }); 
+    if (immedSpouseArray.length > 0) {
+      for (let i = 0; i < immedSpouseArray.length; i++) {
+        alert(immedSpouseArray[i].firstName + " " + immedSpouseArray[i].lastName + " is the spouse of " + person[0].firstName + " " + person[0].lastName + ".")
+      }
+    } else { alert(person[0].firstName + " " + person[0].lastName + " has no spouse.") }
+      break;
     case "info":
       alert(person[0].firstName + person[0].lastName + " is a " + person[0].gender + ", who is " + age + " years old , and is " + person[0].height + " inches tall, aswell as an eye color of " + person[0].eyeColor + " and a weight of " + person[0].weight);
-      break;
-    case "family":
-
-      if (person[0].currentSpouse != null && person[0].parents.length == 2) {
-        alert(person[0].firstName + " " + person[0].lastName + " has a spouse with the id of " + person[0].currentSpouse + ", and parents with the id's of " + person[0].parents[0] + " and " + person[0].parents[1]);
-      } else if (person[0].currentSpouse != null && person[0].parents.length == 1) {
-        alert(person[0].firstName + " " + person[0].lastName + " has a spouse with the id of " + person[0].currentSpouse + ", and a parent with the id of " + person[0].parents[0]);
-      } else if (person[0].currentSpouse != null && person[0].parents.length == 0) {
-        alert(person[0].firstName + " " + person[0].lastName + " has a spouse with the id of " + person[0].currentSpouse + ", and the parents are unknown");
-      } else if (person[0].currentSpouse == null && person[0].parents.length == 2) {
-        alert(person[0].firstName + " " + person[0].lastName + " has parents with the id's of " + person[0].parents[0] + " and " + person[0].parents[1] + ", but has no spouse :( poor " + person[0].firstName + ".");
-      } else if (person[0].currentSpouse == null && person[0].parents.length == 1) {
-        alert(person[0].firstName + " " + person[0].lastName + " has parents with the id's of " + person[0].parents[0] + ", but has no spouse :( poor " + person[0].firstName + ".");
-      } else if (person[0].currentSpouse == null && person[0].parents.length == 0) {
-        alert(person[0].firstName + " " + person[0].lastName + " has unknown parents, and has no spouse. :( Poor " + person[0].firstName + ".");
-      }
-      break;
+      break;      
     case "descendants":
       let peopleToCheck = [person[0]];
       let allDescendants = [];
@@ -168,12 +182,12 @@ function mainMenu(person, people) {
 function searchByName(people) {
 
   var firstName = promptFor("What is the person's first name?", chars);
-    firstName = justCapsFirst(firstName);
+  firstName = justCapsFirst(firstName);
   var lastName = promptFor("What is the person's last name?", chars);
-    lastName = justCapsFirst(lastName);
+  lastName = justCapsFirst(lastName);
 
   var foundPerson = people.filter(function (person) {
-    if (person.firstName === firstName && person.lastName === lastName) {
+    if (person.firstName == firstName && person.lastName == lastName) {
       return true;
     }
     else {
@@ -184,6 +198,7 @@ function searchByName(people) {
   return foundPerson;
 }
 
+/*
 function searchByTrait(people) {
     let chooseTraitOne = promptFor("What is persons the first trait you know? (gender, dob, height, weight, eyecolor, or occupation)", chars)
     chooseTraitOne = justLow(chooseTraitOne)
@@ -253,17 +268,118 @@ function searchByTrait(people) {
       alert("Not a valid input, restarting program :)");
       return app(people);
   }
+  */
 
-  var foundTrait = people.filter(function (person) {
-    if (person[chooseTraitOne] === firstTrait && person[chooseTraitTwo] === secondTrait) {
+//search based on single criterion
+function searchByTrait(people) {
+  let theTrait;
+  let chooseTheTrait = promptFor("What is a trait you would like to search the database for? (gender, dob, height, weight, eye color, or occupation)", chars);
+  chooseTheTrait.toLowerCase();
+
+  switch (chooseTheTrait) {
+    case "gender":
+      theTrait = promptFor("What is the persons gender?", chars);
+      break;
+    case "dob":
+      theTrait = promptFor("What is the persons date of birth?", chars);
+      break;
+    case "height":
+      theTrait = promptFor("What is the persons height?", chars);
+      break;
+    case "weight":
+      theTrait = promptFor("What is the persons weight?", chars);
+      break;
+    case "eyecolor":
+      chooseTheTrait = "eyeColor";
+      theTrait = promptFor("What is the persons eye color?", chars);
+      break;
+    case "occupation":
+      theTrait = promptFor("What is the persons occupation?", chars);
+      break;
+    default:
+      return "not a valid input";
+  }
+  let foundTrait = people.filter(function (person) {
+    if (person[chooseTheTrait] == theTrait) {
       return true;
     }
     else {
       return false;
     }
   })
-  return foundTrait;
+  alert(foundTrait.map(function (person) {
+    return person.firstName + " " + person.lastName;
+  }).join("\n"));
 }
+
+//search based on two to five criterion
+function searchByTraits(people) {
+
+  let arrayLength = promptFor("How many traits would you like to search for?(up to 5)", chars);
+  let traitArray = [];
+
+  for (let i = 0; i < arrayLength; i++) {
+    traitArray.push(promptFor("What is one of the persons traits you know? (gender, height, weight, eyeColor, or occupation)", chars));
+  }
+
+  let traitValueArray = [];
+
+  for (let i = 0; i < traitArray.length; i++) {
+    switch (traitArray[i]) {
+      case "gender":
+        traitValueArray.push(promptFor("What is the persons gender?", chars));
+        break;
+      case "dob":
+        traitValueArray.push(promptFor("What is the persons date of birth?", chars));
+        break;
+      case "height":
+        traitValueArray.push(promptFor("What is the persons height?", chars));
+        break;
+      case "weight":
+        traitValueArray.push(promptFor("What is the persons weight?", chars));
+        break;
+      case "eyeColor":
+        traitValueArray.push(promptFor("What is the persons eye color?", chars));
+        break;
+      case "occupation":
+        traitValueArray.push(promptFor("What is the persons occupation?", chars));
+        break;
+      default:
+        return "not a valid input";
+    }
+  }
+
+  let allTraitsArray = [];
+
+  for (let i = 0; i < traitArray.length; i++){
+  let foundTraits = people.filter(function (person) {
+    if (person[traitArray[i]] == traitValueArray[i]) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  })
+  allTraitsArray.push(foundTraits.map(function (person) {
+    return person.firstName + " " + person.lastName;
+  }));
+}
+
+let result = allTraitsArray.shift().filter(function(el) {
+  return allTraitsArray.every(function(el2) {
+      return el2.indexOf(el) !== -1;
+  });
+});
+
+alert(result);
+}
+
+
+
+
+
+
+
 
 // alerts a list of people
 function displayPeople(people) {
@@ -320,24 +436,26 @@ function chars(input) {
 }
 
 function justCapsFirst(input) {
-    let justLow = input.toLowerCase()
-    let firstCap = justLow.charAt(0).toUpperCase() + justLow.slice(1);
-    return firstCap;
+  let justLow = input.toLowerCase()
+  let firstCap = justLow.charAt(0).toUpperCase() + justLow.slice(1);
+  return firstCap;
 };
 
-
+/*
 function capC(input) {
-    let split = input.split('');
-    split.splice(3, 1, 'C')
-    let joined = split.join('');
-    return joined
+  let split = input.split('');
+  split.splice(3, 1, 'C')
+  let joined = split.join('');
+  return joined
 }
+
 
 function justLow(input) {
-    let lowC =  input.toLowerCase()
-    if (lowC === "eyecolor") {
-        return capC(lowC)
-    } else {
-        return lowC
-    }
+  let lowC = input.toLowerCase()
+  if (lowC === "eyecolor") {
+    return capC(lowC)
+  } else {
+    return lowC
+  }
 }
+*/
